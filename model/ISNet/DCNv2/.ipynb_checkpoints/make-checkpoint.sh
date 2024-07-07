b9b1e245217fc2f47ffcff96cd46a5f5ff58493a
psile <2193738978@qq.com>
2024-07-07 19:27:27 +0800

    2 push

--- model/ISNet/DCNv2/.ipynb_checkpoints/make-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-rm *.so
-rm -r build/
-python3 setup.py build develop
--- model/ISNet/lib/test_script.py
+++
@@ -1,5 +0,0 @@
-from net.backbone import build_backbone
-import torch
-
-net = build_backbone('xception')
-torch.save(net.state_dict(),'/home/yude/project/pretrained/netmodel.pth')
